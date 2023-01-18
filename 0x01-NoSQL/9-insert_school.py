da970119b87152acc2e47b0d69305c2c86b28b3a
Mfon <92741946+themfon@users.noreply.github.com>
2023-01-18 16:37:08 +0100

    NoSQL

---
+++ 0x01-NoSQL/9-insert_school.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+"""
+Inserts a new document in a collection
+"""
+
+
+def insert_school(mongo_collection, **kwargs):
+    """
+    Inserts a new document
+    Parameters:
+    mongo_collection (collection): collection to insert
+    a new document
+    kwargs (dict): new document to insert
+    Returns:
+    Id of new document
+    """
+
+    return mongo_collection.insert_one(kwargs).inserted_id
